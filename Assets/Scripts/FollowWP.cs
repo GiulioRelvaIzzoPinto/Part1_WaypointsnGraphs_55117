b88ab2ea1cece89793841d10fa35c79e78e00977
Giulio <giuliorelvaip@gmail.com>
2024-11-16 00:42:45 +0000

    Commit1 Tank Trackers

---
+++ Assets/Scripts/FollowWP.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWP : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int currentWP = 0;
+
+    public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
+
+    public float LookAhead = 10.0f;
+
+    GameObject tracker;
+
+    void Start()
+    {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > LookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 2) * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        ProgressTracker();
+
+        /*if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;*/
+
+        //this.transform.LookAt(waypoints[currentWP].transform);
+        
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
